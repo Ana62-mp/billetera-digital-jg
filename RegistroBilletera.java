4863080368bee11bc0f967a3f4baad019d26c0ad
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-03-26 19:38:51 -0500

    se crea las constantes

---
+++ RegistroBilletera.java
@@ -0,0 +1,19 @@
+public class RegistroBilletera {
+
+	// Constantes
+
+	private static final int CAPACIDAD_MAXIMA = 10;
+	private static final String MENSAJE_REGISTRO = "Billetera registrada: ";
+	private static final String MENSAJE_ENCONTRADA = "Cuenta encontrada: ";
+	private static final String MENSAJE_NO_ENCONTRADA = "Cuenta no encontrada: ";
+	private static final String SEPARADOR = " - ";
+	private static final String SEPARADOR_PROPIETARIO = " | Propietario: ";
+
+
+
+
+
+
+
+	
+}
