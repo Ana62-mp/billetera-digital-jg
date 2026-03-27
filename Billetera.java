fe370e7657fd3f85c9433fca972af065ecc70f65
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-03-26 19:38:42 -0500

    create private.java

--- Billetera.java
+++ Billetera.java
@@ -1,1 +1,16 @@
+public class Billetera {
+    // Atributos con los tipos exactos pedidos [cite: 215]
+    private String propietario;
+    private String numeroCuenta;
+    private double saldo;
+    private boolean activa;
 
+    // Constructor: activa se inicializa en true automáticamente [cite: 218, 219]
+    public Billetera(String propietario, String numeroCuenta, double saldoInicial) {
+        this.propietario = propietario;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+        this.activa = true; 
+    }
+
+}
