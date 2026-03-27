29dbd3d66f2fb0959231e35b4122281b3c3dd361
Ana62-mp <ana.moposita84@gmail.com>
2026-03-26 20:00:48 -0500

    Refactor Main class to include detailed comments for each component and add RegistroBilletera functionality

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("===== Billetera Digital =====");
         System.out.println();
 
-        // Billetera
+        // INTEGRANTE 1 - Billetera
         Billetera b = new Billetera("Carlos Mendoza", "BW-00123", 500);
         b.mostrarInfo();
         b.depositar(150);
@@ -13,7 +13,7 @@ public class Main {
 
         System.out.println();
 
-        // Transaccion
+        // INTEGRANTE 2 - Transaccion
         Transaccion t = new Transaccion("TRANSFERENCIA", 75, "Pago de servicio");
         t.mostrarDetalle();
         t.aplicarComision(2.5);
@@ -21,11 +21,24 @@ public class Main {
 
         System.out.println();
 
-        // Contacto
+        // INTEGRANTE 3 - Contacto
         Contacto c = new Contacto("Laura Torres", "BW-00456", "BilleteraXYZ");
         c.mostrarContacto();
         c.marcarFavorito();
         c.enviarDinero(120);
+
+        System.out.println();
+
+        // INTEGRANTE 4 - RegistroBilletera
+        RegistroBilletera r = new RegistroBilletera("Banco Digital");
+
+        r.registrarBilletera("BW-00123", "Carlos Mendoza");
+        r.registrarBilletera("BW-00456", "Laura Torres");
+
+        r.buscarCuenta("BW-00123");
+        r.buscarCuenta("BW-99999");
+
+        r.mostrarRegistro();
 
         System.out.println();
         System.out.println("===== Fin del sistema =====");
